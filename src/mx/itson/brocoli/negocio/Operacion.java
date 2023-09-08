d5dd211216245a98f6638d572d80a0cffb1eac4d
galaz <galaz@10.232.8.215>
2023-09-08 10:23:40 -0700

    Verción inicial del proyecto

---
+++ src/mx/itson/brocoli/negocio/Operacion.java
@@ -0,0 +1,20 @@
+
+package mx.itson.brocoli.negocio;
+
+/**
+ * contine metodos para trabajear con tipos de datos string.
+ * @author Lusy
+ */
+public class Operacion {
+    
+    public String [] separar (String valor){
+        
+        /**
+         * separa una cadena de texto a partir de la expresion guion medio.
+         * @param valor cadena de tecto que sera separada a partir de la expresion dada.
+         * @return array de string donde dada posicion representa una division de la cadena de texto.
+         */
+        String[] elementos = valor.split("-");
+        return elementos;
+                }
+}
